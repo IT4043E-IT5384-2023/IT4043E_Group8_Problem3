2f34ec00da569c7da64c88547fa31a4dee4569dc
Richard Ng <85872272+tuanduy-309@users.noreply.github.com>
2023-12-21 18:57:29 +0700

    Add files via upload

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -8,6 +8,7 @@ spark = (SparkSession.builder.appName("group08").master("spark://34.142.194.212:
          .config("spark.jars", "/opt/spark/jars/gcs-connector-latest-hadoop2.jar")
          .config("spark.executor.memory", "2G")  #excutor excute only 2G
         .config("spark.driver.memory","4G") 
+        .config("spark.debug.maxToStringFields", "1000000") 
         .config("spark.executor.cores","1") #Cluster use only 3 cores to excute as it has 3 server
         .config("spark.python.worker.memory","1G") # each worker use 1G to excute
         .config("spark.driver.maxResultSize","3G") #Maximum size of result is 3G
@@ -15,7 +16,7 @@ spark = (SparkSession.builder.appName("group08").master("spark://34.142.194.212:
          .config("spark.port.maxRetries", "100")
          .getOrCreate())
 #config the credential to identify the google cloud hadoop file 
-spark.conf.set("google.cloud.auth.service.account.json.keyfile","/opt/spark/lucky-wall-393304-2a6a3df38253.json")
+spark.conf.set("google.cloud.auth.service.account.json.keyfile","/opt/bucket_connector/lucky-wall-393304-3fbad5f3943c.json")
 spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
 spark._jsc.hadoopConfiguration().set('fs.gs.auth.service.account.enable', 'true')
 
@@ -37,33 +38,9 @@ def data_cleansing(i):
         kols_df = kols_df.withColumn("is_verified_num", when(col("is_verified"), 1).otherwise(0))
     combined_df = kols_df.join(tweets_df, kols_df.user_id == tweets_df.author_id)
     combined_df = combined_df.dropDuplicates()
-    final_df = combined_df.select("user_id", "is_verified_num", "tweet_body", "media_count", "statuses_count", "favourites_count", "friends_count" , "followers_count", "like_counts", "reply_counts", "retweet_counts")
-    
-    # Tokenizer
-    tokenizer = RegexTokenizer(inputCol="tweet_body", outputCol="words", pattern="\\W")
-    tokenized_df = tokenizer.transform(final_df)
-
-    # Remove stop words
-    remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
-    filtered_df = remover.transform(tokenized_df)
-
-    # TF-IDF
-    hashingTF = HashingTF(inputCol="filtered_words", outputCol="rawFeatures")
-    featurized_df = hashingTF.transform(filtered_df)
-    idf = IDF(inputCol="rawFeatures", outputCol="tweet_body_features")
-    rescaled_df = idf.fit(featurized_df).transform(featurized_df)
-
-    # Feature assembler
-    assembler = VectorAssembler(
-        inputCols=["tweet_body_features", "is_verified_num", "media_count", "statuses_count", "favourites_count", "friends_count", "followers_count", "like_counts", "reply_counts", "retweet_counts"],
-        outputCol="combined_features"
-    )
-    combined_df = assembler.transform(rescaled_df)
-    combined_df.show()
-    # Final input
-    df = combined_df.select("combined_features")
-    return df
-
-df = data_cleansing(1)
-df.show()
+    final_df = combined_df.select("user_id", "tweet_body")
+    return final_df
+saved_path = f"gs://it4043e-it5384/it4043e/it4043e_group8_problem3/raw/processed_data/test"
+df = data_cleansing(18)
+df.write.mode("append").json(saved_path)
 spark.stop()
